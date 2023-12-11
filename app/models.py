3b90e2406aeae8336a324e2c1c5fe94dc62a9d0d
Gopal Dnyaneshwar Jogi <gopaljogi5@gmail.com>
2023-12-11 20:56:36 +0530

    Changes to be committed: 	modified:   app/__pycache__/models.cpython-311.pyc 	modified:   app/models.py 	modified:   db.sqlite3

--- app/models.py
+++ app/models.py
@@ -6,7 +6,14 @@ class Countries(models.Model):
     CName=models.CharField(max_length=10,unique=True)
     RegionId=models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.CId
+
 class City(models.Model):
     CityNname=models.CharField(max_length=100)
     PINCode=models.IntegerField(null=True)
     CId=models.ForeignKey(Countries,on_delete=models.CASCADE)
+
+    
+    def __str__(self) -> str:
+        return self.CityNname
